c366354578fa6a44ce43720a93ac51da22e6c76c
perezjav325 <perezjav325@gmail.com>
2024-06-02 18:14:49 -0700

    settings placeholder added

--- main/src/App.js
+++ main/src/App.js
@@ -4,6 +4,8 @@ import Cookies from 'universal-cookie';
 import { Chat } from "./components/Chat";
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 export const cookies = new Cookies();
 
 
@@ -11,19 +13,20 @@ function App() {
   const [isInRoom, setIsInRoom] = useState(cookies.get("user-room"));
   const checkCookie = (index) => {
     var cookie_values = cookies.get("user-room");
-    if(cookie_values != null){cookie_values = cookie_values.split("-");}
-    if(isInRoom) return cookie_values[index];
+    if (cookie_values != null) { cookie_values = cookie_values.split("-"); }
+    if (isInRoom) return cookie_values[index];
     else return null;
   }
 
   const [room, setRoom] = useState(checkCookie(1));
   const [name, setName] = useState(checkCookie(0));
 
+  const [settings, setSettings] = useState("none");
 
   const roomInputRef = useRef(null);
   const nameInputRef = useRef(null);
 
-  const signUserOut = async() =>{
+  const signUserOut = async () => {
     await signOut(auth);
     cookies.remove("user-room");
     setIsInRoom(false);
@@ -31,41 +34,57 @@ function App() {
   }
 
   const enterRoom = () => {
-    if(roomInputRef.current.value == "" || nameInputRef.current.value == "") return;
+    if (roomInputRef.current.value == "" || nameInputRef.current.value == "") return;
     setRoom(roomInputRef.current.value);
     setName(nameInputRef.current.value);
     setIsInRoom(true);
-    cookies.set("user-room", nameInputRef.current.value+"-"+roomInputRef.current.value);
+    cookies.set("user-room", nameInputRef.current.value + "-" + roomInputRef.current.value);
   }
 
   const keyDownFunction = (e) => {
-    if(e.key === 'Enter'){
+    if (e.key === 'Enter') {
       enterRoom();
     }
   }
 
-  return <div> 
-  {isInRoom ? <Chat room={room} name={name} sign_out={signUserOut}/> : 
-
-  <div className="form">
-  <div className="form-header"><h1>skibidi-chat</h1></div>
-  <div className="name-label"> 
-  <label>Display Name</label> 
-  <input maxLength={16} type="text" onKeyDown={keyDownFunction} ref={nameInputRef}/>
-  </div>
- 
-  <div className="name-label">
-  <label>Room Name</label>
-  <input maxLength={16} type="text" onKeyDown={keyDownFunction} ref={roomInputRef}/>
-  </div>
-
-  <button 
-  className="enter-button" 
-  onClick={() => {enterRoom();}}
-  >Enter Room
-  </button>
-  </div>
+  const settingsClick = () => {
+    if (settings === "main") { setSettings("none"); }
+    else { setSettings("main"); }
   }
+
+  return <div>
+    {isInRoom ? <Chat room={room} name={name} sign_out={signUserOut} /> :
+
+      <div className="form">
+        {settings === "none" ?
+          <div className="form-header">
+            <FontAwesomeIcon onClick={settingsClick} className="gear-icon" icon={faGear} />
+            <h1>skibidi-chat</h1>
+          </div>
+          : <div className="form-header" id={settings}>
+            <div className="settings-top">
+              Settings
+              <FontAwesomeIcon onClick={settingsClick} className="caret-up" icon={faCaretUp} />
+            </div>
+            <div className="settings-content">Settings Go Here</div>
+          </div>}
+        <div className="entry-form">
+          <label className="name-label">Display Name</label>
+          <input maxLength={16} type="text" onKeyDown={keyDownFunction} ref={nameInputRef} />
+        </div>
+
+        <div className="entry-form">
+          <label className="name-label">Room Name</label>
+          <input maxLength={16} type="text" onKeyDown={keyDownFunction} ref={roomInputRef} />
+        </div>
+
+        <button
+          className="enter-button"
+          onClick={() => { enterRoom(); }}
+        >Enter Room
+        </button>
+      </div>
+    }
   </div>
 
 }
--- main/src/components/Chat.js
+++ main/src/components/Chat.js
@@ -1,36 +1,37 @@
 import { useEffect, useState } from "react";
-import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy, limit} from 'firebase/firestore';
+import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy, limit } from 'firebase/firestore';
 import { auth, db } from "../firebase-config";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { faGear, faRightFromBracket, faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Chat.css';
 
 export const Chat = (props) => {
-    const {room} = props;
-    const {name} = props;
-    const {sign_out} = props;
+    const { room } = props;
+    const { name } = props;
+    const { sign_out } = props;
 
     const messagesRef = collection(db, "messages");
     const [newMessage, setNewMessage] = useState("");
     const [messages, setMessages] = useState([]);
 
     const delay = ms => new Promise(res => setTimeout(res, ms));
-    
+
+    const [settings, setSettings] = useState("none");
 
     useEffect(() => {
-        const queryMessages = query(messagesRef, where("room", "==", room), orderBy("createdAt", "desc"), limit(100) );
-        const unsubscribe = onSnapshot(queryMessages, async (snapshot) =>{
+        const queryMessages = query(messagesRef, where("room", "==", room), orderBy("createdAt", "desc"), limit(100));
+        const unsubscribe = onSnapshot(queryMessages, async (snapshot) => {
             let msgs = [];
             snapshot.forEach((doc) => {
-                msgs.push({...doc.data(), id: doc.id })
+                msgs.push({ ...doc.data(), id: doc.id })
             });
             var objDiv = document.getElementById("msg_box");
-            if(objDiv.scrollTop >= 0){
+            if (objDiv.scrollTop >= 0) {
                 setMessages(msgs);
                 await delay(100);
                 objDiv.scrollTop = objDiv.scrollHeight;
             }
-            else{setMessages(msgs);}
+            else { setMessages(msgs); }
         });
         return () => {
             unsubscribe();
@@ -40,7 +41,7 @@ export const Chat = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if(newMessage === "") return;
+        if (newMessage === "") return;
 
         await addDoc(messagesRef, {
             text: newMessage,
@@ -54,8 +55,9 @@ export const Chat = (props) => {
         setNewMessage("");
     };
 
-    const settingsClick = (e) => {
-        e.preventDefault();
+    const settingsClick = () => {
+        if (settings === "main") { setSettings("none"); }
+        else { setSettings("main"); }
     }
 
     const leaveRoomClick = (e) => {
@@ -64,37 +66,45 @@ export const Chat = (props) => {
     }
 
     const confirmUser = (msg_user) => {
-        if(msg_user == name){return "self";}
+        if (msg_user == name) { return "self"; }
         else return "other";
     }
-    
+
 
     return (
         <div className="chat-app">
-        <div className="header">
-        <FontAwesomeIcon onClick={settingsClick} className="gear-icon" icon={faGear}/>
-        <FontAwesomeIcon onClick={leaveRoomClick} className="leave-room-button" icon={faRightFromBracket} flip="horizontal" />
-        <h1>{room}</h1>
-        </div>
-        <div className="messages" id="msg_box"> 
-        {messages.map((message)=> (
-            <div className="message" key={message.id}>
-            <span className="user" id={confirmUser(message.name)}>{message.name} :</span>
-            {message.text}
+            {settings === "none" ?
+                <div className="header">
+                    <FontAwesomeIcon onClick={settingsClick} className="gear-icon" icon={faGear} />
+                    <FontAwesomeIcon onClick={leaveRoomClick} className="leave-room-button" icon={faRightFromBracket} flip="horizontal" />
+                    <h1>{room}</h1>
+                </div> : <div className="form-header" id={settings}>
+                    <div className="settings-top">
+                        Settings
+                        <FontAwesomeIcon onClick={settingsClick} className="caret-up" icon={faCaretUp} />
+                    </div>
+                    <div className="settings-content">Settings Go Here</div>
+                </div>
+            }
+            <div className="messages" id="msg_box">
+                {messages.map((message) => (
+                    <div className="message" key={message.id}>
+                        <span className="user" id={confirmUser(message.name)}>{message.name} :</span>
+                        {message.text}
+                    </div>
+                ))}
             </div>
-            ))}
-        </div>
-        <form className="new-message-form" onSubmit={handleSubmit}>
-            <input 
-            className="new-message-input" 
-            placeholder="Enter message..."
-            onChange={(e) => setNewMessage(e.target.value)}
-            value={newMessage}
-            />
-            <button className="send-button" type="submit">
-                Send
-            </button>
-        </form> 
+            <form className="new-message-form" onSubmit={handleSubmit}>
+                <input
+                    className="new-message-input"
+                    placeholder="Enter message..."
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                />
+                <button className="send-button" type="submit">
+                    Send
+                </button>
+            </form>
         </div>
     );
 };
